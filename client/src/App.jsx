5e1df227ae601b2fab6254f7ec05575d703693ef
AshuZed <ashenafizewudie2@gmail.com>
2024-06-15 15:39:01 -0300

    create react app

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import Home from './mainpage/Home'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Home></Home>
+      <h1>hello ermi</h1>
+      
+    </>
+  )
+}
+
+export default App
